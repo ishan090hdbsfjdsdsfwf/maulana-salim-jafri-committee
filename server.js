9151a805aa6e328cc877845d139c09cc5b8c89ce
Ishan Khan <95269062+ishan090hdbsfjdsdsfwf@users.noreply.github.com>
2026-09-12 00:23:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1403,21 +1403,28 @@ app.get(
     </td>
 
 
-    <td>
+<td>
 
-        ${
-            r.createdAt
+    ${
+        r.createdAt
 
-                ? escapeHtml(
-                    new Date(
-                        r.createdAt
-                    ).toLocaleString()
-                )
+            ? escapeHtml(
+                new Date(r.createdAt).toLocaleString('en-IN', {
+                    timeZone: 'Asia/Kolkata',
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hour12: true
+                })
+            )
 
-                : ''
-        }
+            : ''
+    }
 
-    </td>
+</td>
 
 
     <td>
@@ -4159,10 +4166,17 @@ app.get(
                         ? `${baseUrl}/photo/${r.aadhaar}/idproof`
                         : '',
 
-                    r.createdAt
-                        ? new Date(
-                            r.createdAt
-                        ).toLocaleString()
+                                       r.createdAt
+                        ? new Date(r.createdAt).toLocaleString('en-IN', {
+                            timeZone: 'Asia/Kolkata',
+                            day: '2-digit',
+                            month: '2-digit',
+                            year: 'numeric',
+                            hour: '2-digit',
+                            minute: '2-digit',
+                            second: '2-digit',
+                            hour12: true
+                        })
                         : ''
 
                 ]
